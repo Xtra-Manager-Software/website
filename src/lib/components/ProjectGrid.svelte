cedbc779712f09700cde77e63bae9bd6757cac5b
Gustyx-Power <gustiadityamuzaky08@gmail.com>
2026-07-21 22:47:04 +0700

    feat(components): integrate PromoCard into ProjectGrid for featured app display

--- src/lib/components/ProjectGrid.svelte
+++ src/lib/components/ProjectGrid.svelte
@@ -1,5 +1,6 @@
 <script>
     import AppCard from "./AppCard.svelte";
+    import PromoCard from "./PromoCard.svelte";
     import IconAndroid from "~icons/simple-icons/android";
 
     let { projects = [], categories = [], layout = "grid" } = $props();
@@ -18,9 +19,15 @@
 {:else}
     <div class="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-4 md:gap-6 opacity-0 animate-slide-up animation-delay-300">
         {#each projects as project, index}
-            <div style="animation-delay: {index * 50}ms;" class="animate-slide-up opacity-0" style:animation-fill-mode="forwards">
-                <AppCard {project} />
-            </div>
+            {#if index === 0}
+                <div style="animation-delay: {index * 50}ms;" class="animate-slide-up opacity-0 col-span-1 md:col-span-2 lg:col-span-3 mb-4" style:animation-fill-mode="forwards">
+                    <PromoCard {project} />
+                </div>
+            {:else}
+                <div style="animation-delay: {index * 50}ms;" class="animate-slide-up opacity-0" style:animation-fill-mode="forwards">
+                    <AppCard {project} />
+                </div>
+            {/if}
         {/each}
     </div>
 {/if}
